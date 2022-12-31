96578c10cfdafeb0034b64a8d96231349d21f841
TheGuyWhoPorts <121072594+TheGuyWhoPorts@users.noreply.github.com>
2022-12-31 13:30:33 -0500

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1160,7 +1160,7 @@ class PlayState extends MusicBeatState
 		add(botplayTxt);
 		
 var creditTxt = new FlxText(876, 648, 348);
-    creditTxt.text = "PORTED BY\nOswaldGamerYT"; creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
+    creditTxt.text = "PORTED BY OswaldGamerYT"; creditTxt.setFormat(Paths.font("vcr.ttf"), 30, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE,FlxColor.BLACK);
     creditTxt.scrollFactor.set();
     add(creditTxt);
 		if(ClientPrefs.downScroll) {
